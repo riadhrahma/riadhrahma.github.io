cce3338d0495de74f2ed398b8474958788c06c63
riadh <gharbiriadh16@gmail.com>
2020-09-18 16:19:51 +0100

    new deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "03cfde34842db380b77ccb636c38f1a3",
 "/": "03cfde34842db380b77ccb636c38f1a3",
-"main.dart.js": "2c679aa938cabf42a1d4243d644a7c90",
+"main.dart.js": "f9cb8897fb3be09bb6b911469d653020",
 "mainAgora.ts": "a8033152739a49d7a87cf06542ded901",
 "manifest.json": "b1eebabc7dc089a263d3f857a42cc43c",
 "node_modules/agora-rtc-sdk/AgoraRTCSDK.min.js": "d790a4e9add83bff20a321f4be848fd5",
